8b090d173fae2cb7d09924c888c5a948e30a227c
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-08-26 20:51:38 -0500

    refactor: Update function declaration style in introduction

---
+++ Introduccion/funciones.js
@@ -0,0 +1,15 @@
+//Forma 1 - Funciones declarativas
+// function saludar() {
+//     console.log('Hola')
+// }
+
+// saludar()
+
+//Forma 2 - Funciones anónimas
+// const saludar = function() {
+//     console.log('Hola')
+// }
+
+//Forma 3 - Arrow functions
+const saludo = () => { console.log('Hola') }
+saludo()
