81f4b447f3d3ce5c21bc72184c01972aa5744924
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-08-27 21:58:47 -0500

    refactor: Improve variable scoping and add comments in scope.js

--- Introduccion/scope.js
+++ Introduccion/scope.js
@@ -14,6 +14,7 @@ const saludo = () => {
 
 saludo()
 
+
 /* Scope local
     -Las variables locales son accesibles solo dentro de la función donde fueron declaradas.
     -Son variables declaradas dentro de esa función. */
@@ -26,3 +27,20 @@ let obtenerNumeroLetras = (palabra) => {
 // console.log(numeroLetras) // Error: numeroLetras is not defined
 //No podemos acceder a la variable numeroLetras porque es una variable local
 obtenerNumeroLetras('Hola')
+
+
+/* Scope de bloque
+    -Las variables declaradas con let y const tienen un scope de bloque.
+    -Un bloque es todo el código que se encuentra entre llaves {}.
+    -Las variables declaradas con var no tienen scope de bloque, solo tienen scope de función. 
+*/
+
+const acceso = false
+const edad = 21
+if (edad >= 18) {
+    const acceso = true
+    console.log(`Acceso: ${acceso}`)
+}
+
+// console.log(acceso) // Error: acceso is not defined si no declara la variable acceso fuera del bloque
+console.log(`Acceso: ${acceso}`) // Acceso: false
