b34201108e8e8df9a7210db27d94bd913de8a32c
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-08-29 21:22:53 -0500

    feat: Add array manipulation methods to introduction

--- Introduccion/metodosArrays.js
+++ Introduccion/metodosArrays.js
@@ -11,3 +11,38 @@ console.log(colores.length) // 6
     -Convierte un array en una cadena de texto.
 */
 console.log(colores.toString()) // rojo,verde,azul,amarillo,naranja,rosa
+
+/*
+    Metodo join()
+    -Convierte un array en una cadena de texto, pero podemos especificar el separador.
+*/
+console.log(colores.join(' - ')) // rojo - verde - azul - amarillo - naranja - rosa
+console.log(colores.join(' | ')) // rojo | verde | azul | amarillo | naranja | rosa
+
+/*
+    Metodo sort()
+    -Ordena los elementos de un array alfabéticamente.
+*/
+console.log(colores.sort()) // ['amarillo', 'azul', 'naranja', 'rojo', 'rosa', 'verde']
+
+/*
+    Metodo reverse()
+    -Invierte el orden de los elementos de un array.
+*/
+console.log(colores.reverse()) // ['rosa', 'naranja', 'amarillo', 'azul', 'verde', 'rojo']
+
+/*
+    Metodo concat()
+    -Une dos o más arrays.
+*/
+const array1 = ['a', 'b', 'c']
+const array2 = ['d', 'e', 'f']
+const array3 = array1.concat(array2)
+console.log(array3) // ['a', 'b', 'c', 'd', 'e', 'f']
+
+/*
+    Metodo push()
+    -Agrega un elemento al final del array.
+*/
+colores.push('violeta')
+console.log(colores) // ['rojo', 'verde', 'azul', 'amarillo', 'naranja', 'rosa', 'violeta']
