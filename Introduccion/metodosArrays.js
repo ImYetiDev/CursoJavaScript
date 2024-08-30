ec5a0f30ba423e46449c4bcbd09bebc7d6e111a2
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-08-29 21:16:06 -0500

    feat: Add methods for array manipulation in introduction

---
+++ Introduccion/metodosArrays.js
@@ -0,0 +1,13 @@
+const colores = ['rojo', 'verde', 'azul', 'amarillo', 'naranja', 'rosa']
+
+/*
+    Metodo .length
+    -Nos devuelve el número de elementos de un array.
+*/
+console.log(colores.length) // 6
+
+/*
+    Metodo toString()
+    -Convierte un array en una cadena de texto.
+*/
+console.log(colores.toString()) // rojo,verde,azul,amarillo,naranja,rosa
