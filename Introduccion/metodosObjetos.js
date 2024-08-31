77cb5e8fbd953f092f680fc33c9d0b25a084453e
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-08-31 09:59:32 -0500

    feat: Add object methods and examples to metodosObjetos.js

--- Introduccion/metodosObjetos.js
+++ Introduccion/metodosObjetos.js
@@ -2,4 +2,35 @@ const usuario = {
     nombre: 'Juan',
     edad: 24,
     amigos: ['Pedro', 'Maria', 'Ana'],
+    saludo: () => {
+        console.log('Hola!')
+    }
 }
+
+/*
+    Metodos propios de los objetos
+    Los objetos pueden tener metodos personalizados.
+*/
+usuario.saludo() // Hola!
+
+/*
+    Metodo Object.keys()
+    -Devuelve un array con las propiedades del objeto.
+*/
+const propiedades = Object.keys(usuario)
+console.log(propiedades) // ["nombre", "edad", "amigos", "saludo"]
+
+/*
+    Metodo Object.values()
+    -Devuelve un array con los valores de las propiedades del objeto.
+*/
+const valores = Object.values(usuario)
+console.log(valores) // ["Juan", 24, ["Pedro", "Maria", "Ana"], ƒ]
+
+/*
+    Metodo Object.entries()
+    -Devuelve un array con las propiedades y valores del objeto.
+*/
+const propiedadesValores = Object.entries(usuario)
+console.log(propiedadesValores) // [["nombre", "Juan"], ["edad", 24], ["amigos", Array(3)], ["saludo", ƒ]]
+console.log(`El objeto tiene ${propiedadesValores.length} propiedades`) // El objeto tiene 4 propiedades
