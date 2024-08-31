f4014aedb97f766814b4a151d1ae353a6bd4df0d
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-08-30 19:15:04 -0500

    feat: Add forEach() and find() methods to methodsArrays2.js

--- Introduccion/metodosArrays2.js
+++ Introduccion/metodosArrays2.js
@@ -14,3 +14,24 @@ console.log(colores.indexOf('blanco')) // -1
 */
 console.log(colores.lastIndexOf('verde')) // 4 Devuelve el último index de verde
 console.log(colores.lastIndexOf('blanco')) // -1
+
+/* 
+    Metodo forEach()
+    -Ejecuta una función por cada elemento del array.
+*/
+colores.forEach((color, i) => { // color es el parametro que recibe la función
+    console.log(`${color} index (${i})`) // rojo index (0) verde index (1) azul index (2) 
+})
+
+/*
+    Metodo find()
+    -Devuelve el primer elemento que cumpla con la condición.
+    -Si no encuentra ningún elemento, devuelve undefined.
+*/
+nombres = ['Juan', 'Pedro', 'Maria', 'Ana']
+const resultado = nombres.find((nombre) =>{ //Por cada nombre del array ejecuta la función
+    if(nombre[0] === 'M'){
+        return nombre
+    } 
+})
+console.log(resultado) //
