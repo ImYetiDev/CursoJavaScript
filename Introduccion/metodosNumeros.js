0bbeabe3f01624df0ccec6fcd8a2940d1f230da0
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-09-02 21:39:14 -0500

    feat: Add parseInt(), parseFloat(), and Math.random() methods to metodosNumeros.js

--- Introduccion/metodosNumeros.js
+++ Introduccion/metodosNumeros.js
@@ -12,3 +12,22 @@ console.log(numero.toString()) // '123'
 */
 const numeroDecimal = 3.1416
 console.log(numeroDecimal.toFixed(2)) // 3.14
+
+/*
+    Metodo parseInt() y parseFloat()
+    -Convierte un string a numero.
+    -parseInt() convierte a entero.
+    -parseFloat() convierte a decimal.
+*/
+const stringNumero = '123'
+const stringDecimal = '3.1416'
+console.log(parseInt(stringNumero)) // 123
+console.log(parseFloat(stringDecimal)) // 3.1416
+
+/*
+    Math.random()
+    -Devuelve un numero aleatorio entre 0 y 1.
+*/
+
+console.log(Math.random()) // 0.123456789
+console.log(Math.random()) // 0.987654321
