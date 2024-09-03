2c1f0c370f5e06076d24073cf8ec28deadef15ee
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-09-02 21:39:03 -0500

    feat: Add toString() and toFixed() methods to metodosNumeros.js

---
+++ Introduccion/metodosNumeros.js
@@ -0,0 +1,14 @@
+/*
+    Metodo toString()
+    -Convierte un numero a string.
+*/
+const numero = 123
+console.log(numero.toString()) // '123'
+
+/*
+    Metodo toFixed()
+    -Formatea un numero con una cantidad de decimales.
+    -Recibe un parametro: la cantidad de decimales.
+*/
+const numeroDecimal = 3.1416
+console.log(numeroDecimal.toFixed(2)) // 3.14
