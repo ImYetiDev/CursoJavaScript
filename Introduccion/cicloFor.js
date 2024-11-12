15dd125fd761f89fe24e6a63abd8d0756cea64b4
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-11-11 23:30:13 -0500

    Add forEach loop to cicloFor.js for indexed logging of names

--- Introduccion/cicloFor.js
+++ Introduccion/cicloFor.js
@@ -1,1 +1,8 @@
 const nombres = ['Juan', 'Pedro', 'Pablo', 'Luis', 'Carlos'];
+nombres.forEach((nombre, index) => {
+    console.log(`${index + 1}.- ${nombre}`);
+}) // 1.- Juan, 2.- Pedro, 3.- Pablo, 4.- Luis, 5.- Carlos
+
+for(let i = 0; i < 11 ; i++){
+    console.log(i);
+} // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
