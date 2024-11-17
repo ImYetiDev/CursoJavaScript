c314d898e49998304b6a4dbf2dfe925d639a6b40
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-11-17 15:41:45 -0500

    Add class structure example in clases.js with definitions for properties, methods, and constructor

---
+++ Introduccion/clases.js
@@ -0,0 +1,12 @@
+/*
+    Estrucutra de una clase en JavaScript
+    -Definir una clase
+    -Propiedades: son las caracteristicas de la clase (variables)
+    -Metodos: son las acciones que puede realizar la clase (funciones)
+    -Constructor: es un metodo especial que se ejecuta al instanciar la clase
+    -Instanciar una clase
+*/
+
+class Usuario{
+    
+}
