469c42b915597b1644f855d42c3775b7e3d71d8e
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-11-17 13:06:24 -0500

    Add cicloWhile.js with while and do-while loop examples

---
+++ Introduccion/cicloWhile.js
@@ -0,0 +1,18 @@
+let contador = 0;
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
+} // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+/*
+    ciclo Do While
+    La diferencia entre el ciclo while y el ciclo do while es que el ciclo do while se ejecuta al menos una vez, ya que la condición se evalúa al final del ciclo.
+*/
+
+i = 100;
+
+do {
+    console.log(i);
+    i++;
+}
+while (i <= 10); // 100
